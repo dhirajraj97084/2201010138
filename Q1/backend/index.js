e1116bcf0a695a2c187bb6242fabfe431448c06f
Dhiraj  Kumar Tiwari <dhirajraj97084@gmail.com>
2025-09-08 13:22:29 +0530

    okay

--- Q1/backend/index.js
+++ Q1/backend/index.js
@@ -1,14 +1,14 @@
 import express from "express";
-import connectDb from "./utility/Db.js";    
+import connectDb from "./utility/Db.js";
 import cors from "cors";
 import urlRouter from "./routes/url.routes.js";
 
-connectDb();   
-
+connectDb();
+const port = 8000;
 const app = express();
-app.use(cors());            
-app.use(express.json());    
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/", urlRouter);
 
-app.listen(8000, () => console.log("Server running on port 8000"));
+app.listen(port, () => console.log("Server running on port 8000"));
